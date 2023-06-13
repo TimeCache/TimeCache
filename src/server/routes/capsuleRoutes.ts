91db48acf421d77d69a64eface4df70a5aa04014
Melissa Armstrong <mkarmstr@gmail.com>
2023-06-13 16:00:32 -0400

    adjusted pseudocode

--- src/server/routes/capsuleRoutes.ts
+++ src/server/routes/capsuleRoutes.ts
@@ -6,15 +6,15 @@ const router = express.Router();
 router.post('/', async (req, res) => {
     // generate a random password
     // send all of the capsule info to the database: 
-   
-    const capsule = {
-        username: req.body.username,
-        password: password,
-        email: req.body.email,
-        phone: req.body.phone,
 
-    }
-    
+            //      userId INT NOT NULL,
+            //      recipientName VARCHAR(255) NOT NULL,
+            //      recipientEmail VARCHAR(255) NOT NULL,
+            //      recipientPhone VARCHAR(15),
+            //      dueDate TIMESTAMP NOT NULL,
+            //      inputText
+            //      accessCode 
+
     // send the capsule info to s3 with the id of the capsule as a tag
     // activate the capsule countdown via twillio 
 
