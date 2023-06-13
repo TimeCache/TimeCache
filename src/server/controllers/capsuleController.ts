2f06a47e99ba4dd46d879a9baae1b4215bd49b8b
Melissa Armstrong <mkarmstr@gmail.com>
2023-06-13 17:43:39 -0400

    added cron job for twilio SMS functionality

--- src/server/controllers/capsuleController.ts
+++ src/server/controllers/capsuleController.ts
@@ -11,26 +11,69 @@ export const capsuleController = {
 
     generateAccessCode: (req, res, next) => {
         res.locals.accessCode = crypto.randomBytes(16).toString('hex');
+
+
+
         console.log('access code generated!')
         next();
     },
-    
+
     saveToDatabase: async (req, res, next) => {
+
+        // TODO: create a new capsule in the database
+        // TODO: save the new access code from res.locals.accessCode 
+                
         console.log('new capsule saved to database!')
         next();
     },
 
     sendToS3: async (req, res, next) => {
+
+        // TODO: create a new bucket in S3 with the name of the access code(?)
+
         console.log('new capsule sent to S3!')
         next();
     },
 
     activateCountdown: async (req, res, next) => {
+
+        // TODO: create a new capsule in the database
+        const { recipientPhone, dueDate, id: capsuleId } = req.body; 
+
+        // set up a cron job
+        const task = cron.schedule(`0 * * *`, async () => {
+          const now = new Date();
+
+          if (now >= new Date(dueDate)) {
+
+            // send the SMS
+            await twilioClient.messages.create({
+              body: `You've recieved a TimeCache time capsule! Access it with this code: ${req.accessCode}`,
+              from: process.env.TWILIO_PHONE_NUMBER,
+              to: recipientPhone
+            });
+
+            // Stop the cron job when the due date has been reached
+            task.destroy();  
+
+            // update the status of the capsule in the database
+            const updateQuery = 'UPDATE timeCapsules SET status = $1 WHERE id = $2';
+            await pool.query(updateQuery, ['sent', capsuleId]);
+          }
+        }, {
+          scheduled: false
+        });
+
+          // Start the cron job
+        task.start();
+
         console.log('countdown activated!')
         next();
-      }
+    }
 
 }
+
+
 
 
 
--- src/server/routes/capsuleRoutes.ts
+++ src/server/routes/capsuleRoutes.ts
@@ -6,14 +6,27 @@ import { capsuleController } from '../controllers/capsuleController';
 const router = express.Router();
 
 router.get('/', capsuleController.getMyCapsule, async (req, res) => { 
-    
-}
 
+})
+
+// create a new time_capsule
 router.post('/', capsuleController.generateAccessCode, capsuleController.saveToDatabase, capsuleController.sendToS3, capsuleController.activateCountdown, async (req, res) => {
     // generate a random password
     // send all of the capsule info to the database
     // send the capsule info to s3 with the id of the capsule as a tag
     // activate the capsule countdown via twillio
+
+    // table info needed: 
+        // userId INT NOT NULL,
+        // capsuleName VARCHAR(255) NOT NULL
+        // recipientName VARCHAR(255) NOT NULL,
+        // recipientEmail VARCHAR(255) NOT NULL,
+        // recipientPhone VARCHAR(15),
+        // dueDate TIMESTAMP NOT NULL,
+        // inputText VARCHAR(255),
+        // accessCode VARCHAR(255) NOT NULL,
+        // status VARCHAR(255) DEFAULT 'pending';
+        // FOREIGN KEY (userId) REFERENCES users(id)
 
     res.json({ message: 'Time capsule created!' });
   });
