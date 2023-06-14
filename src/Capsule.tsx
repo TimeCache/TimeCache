24a154a6ff2486b3caeb4d33c8634248d128fa02
HWein8 <hadariweinstein@gmail.com>
2023-06-14 16:32:42 -0400

    added S3 bucket functionality

--- src/Capsule.tsx
+++ src/Capsule.tsx
@@ -1,60 +1,47 @@
-import React = require("react")
-import { useState } from "react";
+import React, { useState } from 'react';
 import './index.css'
 
 export default function Capsule() {
-
   const [selectedFiles, setSelectedFiles] = useState([]);
+  // some stuff
 
-  async function submit(event: any){
+  const handleFileChange = (event: any) => {
+
+    setSelectedFiles([...event.target.files]);
+    console.log(selectedFiles)
+  };
+
+  function submit(event: any){
+    const formData = new FormData(event.target);
     event.preventDefault()
-
-    const formData = new FormData(event.target)
+    const uploadData = new FormData()
 
     const capsuleData = {}
-
     for (const value of formData.entries()) {
       if (value[0] !== 'file') {
         capsuleData[value[0]] = value[1]
-      }
+      } 
     }
 
-    try {
+    selectedFiles.forEach((file) => {
+      uploadData.append('files', file);
+    });
 
-      await fetch('/capsules', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(capsuleData)
+
+
+
+    fetch('/capsules', {
+      method: 'POST',
+      body: uploadData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
       })
-
-    } catch(error) {
-      console.log(`${error} in the POST for capsules`)
-    }
-
-    // const handleFileChange = (event: any) => {
-    //   setSelectedFiles([...event.target.files]);
-    //   console.log(selectedFiles)
-    // };
-
-    // const handleUpload = () => {
-    //   const formData = new FormData();
-    //   selectedFiles.forEach((file) => {
-    //     formData.append('files', file);
-    //   });
-    //   fetch('/capsules/upload', {
-    //     method: 'POST',
-    //     body: formData,
-    //   })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       console.log(data);
-    //     })
-    //     .catch((error) => {
-    //       console.error('Error uploading files:', error);
-    //     });
-    // };
-
-    
+      .catch((error) => {
+        console.error('Error uploading files:', error);
+      });
+    // console.log(event.target.name.value, event.target.setDate.value)
   }
 
   return (
@@ -63,37 +50,25 @@ export default function Capsule() {
         <form onSubmit={submit} className="join join-vertical">
           <div className="p-3">
         <label className="font-mono">
-            Capsule Name
-            <input type="text" name="capsuleName" className="input input-bordered input-secondary w-full max-w-xs"></input>
-          </label>
-          </div>
-          <div className="p-3">
-        <label className="font-mono">
             Recipient Name
-            <input type="text" name="recipientName" className="input input-bordered input-secondary w-full max-w-xs"></input>
-          </label>
-          </div>
-          <div className="p-3">
-        <label className="font-mono">
-          Recipient Phone
-            <input type="tel" name="phone" className="input input-bordered input-secondary w-full max-w-xs"></input>
+            <input type="text" id="name" className="input input-bordered input-secondary w-full max-w-xs"></input>
           </label>
           </div>
           <div className="p-3">
           <label className="font-mono">
             Message
-            <input type="text" name="message" className="input input-bordered input-secondary w-full max-w-xs"></input>
+            <input type="text" id="message" className="input input-bordered input-secondary w-full max-w-xs"></input>
           </label>
           </div>
           <div className="p-3">
           <label className="font-mono">
             Set Date
-            <input type="date" name="setDate" className="input input-bordered input-secondary w-full max-w-xs"></input>
+            <input type="date" id="setDate" className="input input-bordered input-secondary w-full max-w-xs"></input>
           </label>
           </div>
           <div className="p-3">
           <label className="font-mono">
-            <input type="file" multiple name="file" className="file-input file-input-bordered file-input-secondary w-full max-w-xs"></input>
+            <input type="file" id="file" multiple onChange={handleFileChange} className="file-input file-input-bordered file-input-secondary w-full max-w-xs"></input>
           </label>
           </div>
           <div className="p-3">
